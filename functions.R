170d71edd341c7e7b8e3f8960a110f982533ca1c
Raphael Saldanha <rfsaldanha@gmail.com>
2023-08-21 17:37:38 +0200

    Update function.

--- functions.R
+++ functions.R
@@ -7,13 +7,19 @@ node_name <- function(){
 }
 
 # Data dir reference, depending on node
-data_dir <- function(){
+data_dir <- function(dest){
   node_name <- node_name()
   
   if(node_name == "fatnode"){
     ref <- "/data/raphaelfs/dengue_data"
   } else {
     ref <- NULL
+  }
+  
+  if(is.null(ref)){
+    ref <- file.path(dest)
+  } else {
+    ref <- file.path(ref, dest)
   }
   
   return(ref)
